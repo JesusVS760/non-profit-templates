43cf0d4c271cdbf6b910466bb982b437d39aa0d7
JesusVA760 <tonio4340406@gmail.com>
2024-06-10 16:41:40 -0700

    about

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "./Landing.css";
+import student from "../assets/images/student.jpg";
 // import img from "../assets/images/landing2.jpg";
 
 const Landing = () => {
@@ -15,7 +16,32 @@ const Landing = () => {
             tellus sit amet neque iaculis posuere. Fusce eget libero nec eros
             tincidunt laoreet.
           </p>
-          <p></p>
+          <div className="landing-buttons">
+            <button>About YE</button>
+            <button>Get in Touch</button>
+          </div>
+        </div>
+      </div>
+      <div className="landing-featured-story">
+        <div className="landing-featured-story-img">
+          <img src={student} alt="student"></img>
+          <div className="span-info">
+            <span>"I was able to reach my goals and dreams"</span>
+            <span>-John Smith, San Diego</span>
+          </div>
+        </div>
+        <div className="landing-featured-content">
+          <p className="story-line">Making a change</p>
+          <h1>Why Start Contributing?</h1>
+          <p className="story-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+            justo vel odio rhoncus porta. Duis consequat libero at velit
+            faucibus, sed semper ligula suscipit. Nunc aliquam est vel efficitur
+            porta. Aliquam sit amet convallis ipsum. Integer convallis, risus
+            vel tincidunt finibus, dui lectus dapibus mauris, sed bibendum enim
+            lectus non dolor.
+          </p>
+          <button className="more-button">More About My Story!</button>
         </div>
       </div>
     </div>
